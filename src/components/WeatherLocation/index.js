4a3d65cc99c751d051813d147ee8571ad2cb4f4e
Germán Pla <germanps@outlook.es>
2018-03-05 17:09:49 +0100

    Convert-units Temperature

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import { WINDY } from './../../constants/weathers';
+import { SUN } from './../../constants/weathers';
 import './weather_location.css';
 
-const location = "Barcelona,cat";
+const location = "Barcelona, es";
 const api_key = "4af7f8fc3c632f3a83020599a9af7fc1";
 const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${api_key}`;
 const data1 = {
   temperature: 18,
-  weatherState: WINDY,
+  weatherState: SUN,
   humidity: 5,
   wind: '20m/s',
 };
@@ -24,16 +25,36 @@ class WeatherLocation extends Component{
     } 
   }
 
+  getTemp = kelvin => {
+    return convert(kelvin).from('K').to('C');
+  }
+  getWeatherState = (weather) => {
+    return SUN;
+  }
+
+  getData = (weather_data) => {
+    const { humidity, temp } = weather_data.main;
+    const { speed } = weather_data.wind;
+    const weatherState = this.getWeatherState(this.weather);
+    const temperature = this.getTemp(temp);
+    const data = {
+      humidity,
+      temperature,
+      weatherState,
+      wind: `${speed} m/s`,
+    }
+    return data;
+  }
+
   handleUpdateClick = () => {
     fetch(api_weather).then( data => {
       console.log(data);
       return data.json();
     }).then( weather_data => {
+      const data = this.getData(weather_data);
+      this.setState({ data: data });
       console.log(weather_data);
     });
-    /* this.setState({
-      data: data2
-    }); */
     console.log("Actualizado!");
   }
 
