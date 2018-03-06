45ab2aa7118fc008419a9ac3e54ab9ce8c568ae7
germanps <gpla@pasiona.com>
2018-03-06 07:54:43 +0100

    Refactoring functions services

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import transformWeather from './../../services/transformWeather';
 import { SUN } from './../../constants/weathers';
 import './weather_location.css';
 
@@ -9,7 +9,7 @@ const location = "Barcelona, es";
 const api_key = "4af7f8fc3c632f3a83020599a9af7fc1";
 const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${api_key}`;
 const data1 = {
-  temperature: 18,
+  temperature: 18.5,
   weatherState: SUN,
   humidity: 5,
   wind: '20m/s',
@@ -25,33 +25,12 @@ class WeatherLocation extends Component{
     } 
   }
 
-  getTemp = kelvin => {
-    return convert(kelvin).from('K').to('C');
-  }
-  getWeatherState = (weather) => {
-    return SUN;
-  }
-
-  getData = (weather_data) => {
-    const { humidity, temp } = weather_data.main;
-    const { speed } = weather_data.wind;
-    const weatherState = this.getWeatherState(this.weather);
-    const temperature = this.getTemp(temp);
-    const data = {
-      humidity,
-      temperature,
-      weatherState,
-      wind: `${speed} m/s`,
-    }
-    return data;
-  }
-
   handleUpdateClick = () => {
     fetch(api_weather).then( data => {
       console.log(data);
       return data.json();
     }).then( weather_data => {
-      const data = this.getData(weather_data);
+      const data = transformWeather(weather_data);
       this.setState({ data: data });
       console.log(weather_data);
     });
---
+++ src/services/transformWeather.js
@@ -0,0 +1,25 @@
+import convert from 'convert-units';
+import { SUN } from './../constants/weathers';
+
+const getTemp = kelvin => {
+  return convert(kelvin).from('K').to('C').toFixed(1);
+}
+const getWeatherState = (weather) => {
+  return SUN;
+}
+
+const transformWeather = weather_data => {
+  const { humidity, temp } = weather_data.main;
+  const { speed } = weather_data.wind;
+  const weatherState = getWeatherState(this.weather);
+  const temperature = getTemp(temp);
+  const data = {
+    humidity,
+    temperature,
+    weatherState,
+    wind: `${speed} m/s`,
+  }
+  return data;
+}
+
+export default transformWeather;
